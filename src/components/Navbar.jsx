4f77515b66540ba57403719c394cb487b1cbc440
VLotito <vincenza.lotito95@outlook.it>
2024-09-06 10:03:45 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return(
+<nav className="bg-black text-white py-5">
+<ul className="flex justify-center gap-5">
+  <li>Home</li>
+  <li>Movies</li>
+  <li>About us</li>
+</ul>
+</nav>
+
+    );
+
+}
